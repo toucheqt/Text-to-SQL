92164074ff203fe61128653eb9535f861fa5ce2c
Ondrej Krpec <ondrej.krpec@gmail.com>
2024-09-22 18:48:15 +0200

    Initialize Maven project

---
+++ src/main/java/cz/ondrejkrpec/texttosql/TextToSqlApplication.java
@@ -0,0 +1,13 @@
+package cz.ondrejkrpec.texttosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextToSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextToSqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/ondrejkrpec/texttosql/TextToSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.ondrejkrpec.texttosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextToSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
